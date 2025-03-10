b23d565baea8eaa0af8f70bb26f36d579d21dae4
Rajashree Sabale <114015610+RCS007@users.noreply.github.com>
2025-03-10 17:35:26 +0530

    Add files via upload

---
+++ Question 1.py
@@ -0,0 +1,10 @@
+# Question 1
+
+# Ask the user to enter their first name and then display the length of their name.
+
+# Ask the user to enter their first name
+first_name = input("Enter your first name: ")
+
+# Calculate and display the length of the name
+name_length = len(first_name)
+print(f"Your name has {name_length} characters.")
